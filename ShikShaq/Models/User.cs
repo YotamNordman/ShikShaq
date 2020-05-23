d3bd9ab89cf7dfcc6c943bb222f91e4ba6fe9f6f
Shaked <shvshv44@gmail.com>
2020-05-23 22:37:30 +0300

    feat: add login page

---
+++ ShikShaq/Models/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ShikShaq.Models
+{
+    public class User
+    {
+
+        [Key]
+        private long id { get; set; }
+        private string name { get; set; }
+        private string email { get; set;}
+        private DateTime birthday { get; set; }
+        private String address { get; set; }
+        private float height { get; set; }
+        private float weigth { get; set; }
+        private String password { get; set; }
+        private bool isAdmin { get; set; }
+
+    }
+}
