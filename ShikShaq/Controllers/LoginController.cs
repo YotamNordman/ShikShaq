4a0f319a7204e2c73a3c61b17ce451e63371a22b
Shaked <shvshv44@gmail.com>
2020-05-26 20:00:52 +0300

    feat: add login page

---
+++ ShikShaq/Controllers/LoginController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ShikShaq.Controllers
+{
+    public class LoginController : Controller
+    {
+
+        private ShikShaqContext _context;
+
+        public LoginController ()
+        {
+            _context = new ShikShaqContext();
+            ViewData["loginError"] = "1234"; //TODO: REMOVE
+        }
+
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Autherize(user loginUser)
+        {
+           
+            using (_context)
+            {
+                var user = (from usr in _context.user
+                            where usr.email == loginUser.email && usr.password == loginUser.password
+                            select usr)
+                            .FirstOrDefault<user>();
+
+                if (user == null)
+                {
+                    ViewData["loginError"] = "User does not exist!";
+                } else
+                {
+                    ViewData["loginError"] = "";
+                }
+            }
+
+
+
+            return RedirectToAction("Index");
+        }
+    }
+}
