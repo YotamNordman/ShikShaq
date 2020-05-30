b5b794a39493642db03d4534022d0d7a67b4cc38
Shaked <shvshv44@gmail.com>
2020-05-30 10:10:38 +0300

    feat: finish dropdown navbar + new account controller

--- ShikShaq/Controllers/AccountController.cs
+++ ShikShaq/Controllers/AccountController.cs
@@ -6,34 +6,34 @@ using Microsoft.AspNetCore.Mvc;
 using ShikShaq.Data;
 using WebApplication1.Models;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 
 namespace ShikShaq.Controllers
 {
-    public class LoginController : Controller
+    public class AccountController : Controller
     {
         private readonly ShikShaqContext _context;
 
-        public LoginController(ShikShaqContext context)
+        public AccountController(ShikShaqContext context)
         {
             _context = context;
         }
 
-        // GET: Login
-        public ActionResult Index()
+        public ActionResult Login()
         {
             return View();
         }
 
         [HttpPost]
-        public ActionResult Index(User loginUser)
+        public async Task<ActionResult> Login(User loginUser)
         {
 
             using (_context)
             {
-                var user = (from usr in _context.User
-                            where usr.Email == loginUser.Email && usr.Password == loginUser.Password
-                            select usr)
-                            .FirstOrDefault<User>();
+                var user = await (from usr in _context.User
+                                  where usr.Email == loginUser.Email && usr.Password == loginUser.Password
+                                  select usr)
+                                  .FirstOrDefaultAsync<User>();
 
                 if (user == null)
                 {
@@ -52,5 +52,16 @@ namespace ShikShaq.Controllers
 
             return View();
         }
+
+        public IActionResult Logout()
+        {
+            // USER IS LOGGED OUT!!
+            HttpContext.Session.SetInt32("userId", -1);
+            HttpContext.Session.SetString("userName", "");
+            HttpContext.Session.SetString("isUserAdmin", "N");
+
+            return RedirectToAction("Index", "Home");
+        }
+
     }
 }
