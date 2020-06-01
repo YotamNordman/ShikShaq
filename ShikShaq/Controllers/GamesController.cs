b2815f6ead9db9e193c008a25b90b99091a6b22d
yarivg <yarivgavriel2@gmail.com>
2020-06-02 02:56:54 +0300

    Add games contoller that return our default index.cshtml view. This view contains an api call (using ajax) to our contoller that gives him last premier league games.. Then he manipulate the data in the client side to display it using jquery data. Added animation every api calls

--- ShikShaq/Controllers/GamesController.cs
+++ ShikShaq/Controllers/GamesController.cs
@@ -10,12 +10,17 @@ namespace ShikShaq.Controllers
 {
     public class GamesController : Controller
     {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
         public async Task<String> Scores()
         {
             // TODO: use this only before showing menachem the project... 
             // Since yariv may be paying for that
 
-            //var client = new RestClient($"https://api-football-v1.p.rapidapi.com/v2/fixtures/league/524/last/10");
+            //var client = new RestClient($"https://api-football-v1.p.rapidapi.com/v2/fixtures/league/524/last/100");
             //var request = new RestRequest(Method.GET);
             //request.AddHeader("x-rapidapi-host", "api-football-v1.p.rapidapi.com");
             //request.AddHeader("x-rapidapi-key", "3c5d9d988amsh4aa8980944ef4f2p118e80jsn3713bac3c462");
