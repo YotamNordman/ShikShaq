cdf63f17b15c11e6825ac915f8004d815e162b73
yarivg <yarivgavriel2@gmail.com>
2020-06-06 16:59:08 +0300

    Take out JS from html files

---
+++ ShikShaq/wwwroot/js/about.js
@@ -0,0 +1,79 @@
+﻿var map;
+initCanvas();
+// Adds a marker to the map.
+function addMarker(location, map, title) {
+    // Add the marker at the clicked location, and add the next-available label
+    // from the array of alphabetical characters.
+    var marker = new google.maps.Marker({
+        position: location,
+        animation: google.maps.Animation.DROP,
+        title: title,
+        map: map
+    });
+
+    marker.setMap(map);
+    marker.addListener('click', toggleBounce);
+}
+
+function toggleBounce() {
+    if (this.getAnimation() !== null) {
+        this.setAnimation(null);
+    } else {
+        this.setAnimation(google.maps.Animation.BOUNCE);
+    }
+}
+
+function initMap() {
+    // Center map at college of management
+    mapCenter = { lat: 31.969738, lng: 34.7749759 };
+    mapCenter2 = { lat: 31.979838, lng: 34.7648759 };
+
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: mapCenter,
+        zoom: 15
+    });
+
+    getBranchesLocations();
+};
+
+function getBranchesLocations() {
+    $.ajax({
+        type: "GET",
+        url: '../Branches/Location',
+        data: {},
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        beforeSend: function () {
+        },
+        success: function (response) {
+            var branchLocation = {};
+            for (branchIndex in response) {
+                branchLocation = {
+                    lat: response[branchIndex].lat,
+                    lng: response[branchIndex].lng
+                };
+
+                addMarker(branchLocation, map, response[branchIndex].name);
+            }
+        },
+        complete: function () {
+        },
+        failure: function (jqXHR, textStatus, errorThrown) {
+            alert("HTTP Status: " + jqXHR.status + "; Error Text: " + jqXHR.responseText); // Display error message
+        }
+    });
+}
+
+function initCanvas() {
+    var c = document.getElementById("titleCanvas");
+    var ctx = c.getContext("2d");
+
+    ctx.font = "55px Verdana";
+    // Create gradient
+    var gradient = ctx.createLinearGradient(0, 0, c.width, 0);
+    gradient.addColorStop("0", "#090979");
+    gradient.addColorStop("1.0", "#00d4ff");
+    // Fill with gradient
+    ctx.fillStyle = gradient;
+    ctx.fillText("Shik-Shaq", 10, 90);
+}
---
+++ ShikShaq/wwwroot/js/games.js
@@ -0,0 +1,62 @@
+﻿var $;
+
+function Show() {
+    $('#div_Loader').show();
+}
+
+function Hide() {
+    $('#div_Loader').hide();
+}
+
+function getScores() {
+    $.ajax({
+        type: "GET",
+        url: 'Games/Scores',
+        data: {},
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        beforeSend: function () {
+            Show(); // Show loader icon
+        },
+        success: function (response) {
+            columns = [
+                { title: "Date" },
+                { title: "Home Team" },
+                { title: "Away Team" },
+                { title: "Score" },
+                { title: "Status" }
+            ];
+
+            scoresDatasource = [];
+
+            for (index in response.api.fixtures) {
+                var fixture = response.api.fixtures[index];
+                scoresDatasource.push([
+                    new Date(fixture.event_date).toLocaleString(),
+                    fixture.homeTeam.team_name,
+                    fixture.awayTeam.team_name,
+                    fixture.score.fulltime,
+                    fixture.status
+                ]);
+            }
+
+            $('#scoresTable').DataTable({
+                data: scoresDatasource,
+                columns: columns
+            });
+        },
+        complete: function () {
+            Hide(); // Hide loader ic
+        },
+        failure: function (jqXHR, textStatus, errorThrown) {
+            alert("HTTP Status: " + jqXHR.status + "; Error Text: " + jqXHR.responseText); // Display error message
+        }
+    });
+}
+
+$(document).ready(function () {
+    $("#div_Loader").show();
+    setTimeout(() => {
+        getScores();
+    }, 2000);
+});
