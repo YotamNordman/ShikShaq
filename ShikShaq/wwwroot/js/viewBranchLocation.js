9d0675a64e371c9954a8780af86a4838158b7ac5
yarivg <yarivgavriel2@gmail.com>
2020-06-13 14:49:48 +0300

    Support viewing location of branch using maps api

---
+++ ShikShaq/wwwroot/js/viewBranchLocation.js
@@ -0,0 +1,30 @@
+﻿function createMarker(location, map, title) {
+    var marker = new google.maps.Marker({
+        position: location,
+        animation: google.maps.Animation.DROP,
+        title: title,
+        map: map
+    });
+
+    marker.setMap(map);
+
+    return marker;
+}
+
+function initMap() {
+    var branchLocation = { lat: 0, lng: 0 };
+
+    if ($("#branchLat").text() != "" && $("#branchLng").text() != "") {
+        branchLocation = {
+            lat: parseFloat($("#branchLat").text()),
+            lng: parseFloat($("#branchLng").text())
+        };
+    }
+
+    var map = new google.maps.Map(
+        document.getElementById('map'),
+        { zoom: 15, center: branchLocation }
+    );
+
+    createMarker(branchLocation, map, $("#branchName").val());
+}
