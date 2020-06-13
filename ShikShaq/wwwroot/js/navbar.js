d5448e7356ca35bf71b46f778a8760aa8db26d86
yarivg <yarivgavriel2@gmail.com>
2020-06-13 14:27:01 +0300

    Fixing a null point exception bug

--- ShikShaq/wwwroot/js/navbar.js
+++ ShikShaq/wwwroot/js/navbar.js
@@ -10,12 +10,15 @@
 window.onscroll = function () { myFunction2() };
 
 var navbar = document.getElementById("myTopnav");
-var sticky = navbar.offsetTop;
+
+var sticky = navbar ? navbar.offsetTop : 0;
 
 function myFunction2() {
-    if (window.pageYOffset >= sticky) {
-        navbar.classList.add("sticky");
-    } else {
-        navbar.classList.remove("sticky")
+    if (navbar) {
+        if (window.pageYOffset >= sticky) {
+            navbar.classList.add("sticky");
+        } else {
+            navbar.classList.remove("sticky")
+        }
     }
 }
