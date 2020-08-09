9fab5efed2a2a648aa234be361fa8e70ad63f2c3
Yariv Gavriel <yarivgavriel2@gmail.com>
2020-08-09 17:24:06 +0300

    the really last commit

--- ShikShaq/Controllers/GamesController.cs
+++ ShikShaq/Controllers/GamesController.cs
@@ -17,17 +17,6 @@ namespace ShikShaq.Controllers
 
         public async Task<String> Scores()
         {
-            // TODO: use this only before showing menachem the project... 
-            // Since yariv may be paying for that
-
-            //var client = new RestClient($"https://api-football-v1.p.rapidapi.com/v2/fixtures/league/524/last/100");
-            //var request = new RestRequest(Method.GET);
-            //request.AddHeader("x-rapidapi-host", "api-football-v1.p.rapidapi.com");
-            //request.AddHeader("x-rapidapi-key", "3c5d9d988amsh4aa8980944ef4f2p118e80jsn3713bac3c462");
-            //request.AddHeader("useQueryString", "true");
-
-            //IRestResponse response = await client.ExecuteAsync(request);
-            //return response.Content.ToString();
 
             String scoresResult = "";
 
